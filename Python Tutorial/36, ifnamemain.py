61c763ba4c7aaee335fbabeb444f463406cad856
coder-jsasano <0rgina1june@gmail.com>
2024-06-12 16:01:38 +0900

    Delete Python Tutorial directory

--- Python Tutorial/36, ifnamemain.py
+++
@@ -1,7 +0,0 @@
-#if __name__ =='__main__'
-
-#Python interpreter sets "special variables", one of which is __name__
-#Python will assign the __name?? variable a value of '__main__' if it is the initial module being run
-
-print(__name__)
-
